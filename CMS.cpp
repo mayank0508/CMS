26cfb9ccf6bc61e39107f855ad5a2c6569ddddf0
mayank0508 <thakurmayank0508@gmail.com>
2021-01-07 01:16:06 +0530

    new

---
+++ CMS.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cstdlib>
+#include<cstring>
+#include<cstdio>
+#include<fstream>
+#include<conio.h>
+char un[20];
+class Admin
+{
+     char name[20];
+     int totsub;
+     char subject[10][10];
+     char mobile [15], mail[50], fname[20];
+     char password[20], rpasswd[20];
+     public:
+     char rollno[15];
+
